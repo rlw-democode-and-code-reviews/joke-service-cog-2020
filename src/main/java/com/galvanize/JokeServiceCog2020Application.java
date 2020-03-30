55e8b3b51a601ee0a27c7f70b8574f74331193d5
rlwing <rob@wingjr.com>
2020-03-30 16:49:28 -0700

    initial commit

---
+++ src/main/java/com/galvanize/JokeServiceCog2020Application.java
@@ -0,0 +1,13 @@
+package com.galvanize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokeServiceCog2020Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JokeServiceCog2020Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/galvanize/JokeServiceCog2020ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galvanize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokeServiceCog2020ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
